42e21a4128ec174bddb1a78b3b02ff31724976b1
glasscharlie <charlie-glass@live.com>
2019-12-04 16:14:37 -0800

    changed code to use binary search

--- array-binary-search/array_binary_search.py
+++ array-binary-search/array_binary_search.py
@@ -1,8 +1,13 @@
-def binary_search(lst, val):
-  i = 0
-  for x in lst:
-    if x == val:
-      return i
-    i += 1
-  else:
-    return -1
+def binary_search(arr, list):
+    min = 0
+    max = len(arr) - 1
+    while True:
+        if max < min:
+            return -1
+        mid = (min + max) // 2
+        if arr[mid] < list:
+            min = mid + 1
+        elif arr[mid] > list:
+            max = mid - 1
+        else:
+            return mid
