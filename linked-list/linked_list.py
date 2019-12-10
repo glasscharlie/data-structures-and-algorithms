592e94251df4f614987db6dd5e2f34e27b0ac34f
glasscharlie <charlie-glass@live.com>
2019-12-10 08:09:02 -0800

    finished challenge 06

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -5,7 +5,7 @@ class Node:
 class linked_list:
     def __init__(self):
         self.head = Node()
-    def insert(self, data):
+    def insert_before(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
@@ -27,3 +27,21 @@ class linked_list:
             cur = cur.next
         print(value)
         return value
+
+    def insert_after(self, desired_location):
+        new_node = Node(desired_location)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def append(self, val):
+        current = self.head
+        while current:
+            if current.next == None:
+                current.next = Node(val)
+                return self.__str__()
+            else:
+                current = current.next
+                self.head = Node(val)
+                return self.__str__()
--- linked-list/test_linked_list.py
+++ linked-list/test_linked_list.py
@@ -11,48 +11,60 @@ def test_make_node():
 
 def test_includes_true():
     expected = True
-    my_list.insert(12)
+    my_list.insert_before(12)
     actual = my_list.includes(12)
     assert actual == expected
 
 def test_includes_false():
     my_list = linked_list()
     expected = False
-    my_list.insert(12)
+    my_list.insert_before(12)
     actual = my_list.includes(13)
     assert actual == expected
 
 def test_to_string_one():
     expected = '  3 2 1'
     my_list = linked_list()
-    my_list.insert(1)
-    my_list.insert(2)
-    my_list.insert(3)
+    my_list.insert_before(1)
+    my_list.insert_before(2)
+    my_list.insert_before(3)
     actual = my_list.to_string()
     assert actual == expected
 
 def test_to_string_two():
     expected = '  1'
     my_list = linked_list()
-    my_list.insert(1)
+    my_list.insert_before(1)
     actual = my_list.to_string()
     assert actual == expected
 
 def test_insert_one():
     expected = 3
-    my_list.insert(3)
+    my_list.insert_before(3)
     my_list.to_string()
     actual = my_list.head.data
     assert actual == expected
 
 def test_insert_two():
     expected = 5
-    my_list.insert(3)
-    my_list.insert(4)
-    my_list.insert(5)
+    my_list.insert_before(3)
+    my_list.insert_after(4)
+    my_list.insert_before(5)
     my_list.to_string()
     actual = my_list.head.data
     assert actual == expected
+
+
+def test_append_one(empty):
+    expected = "z"
+    empty.append('z')
+    assert empty.includes('z')
+
+
+def test_append_two(lst):
+    expected = "a, b, c, d, e"
+    lst.append('e')
+    assert lst.includes('e')
 
 
 
