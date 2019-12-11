a7adef22111e11223321852cc2b247162429a6d0
glasscharlie <charlie-glass@live.com>
2019-12-10 18:07:40 -0800

    finished all the linked-list challenge problems

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -1,21 +1,21 @@
 class Node:
-    def __init__(self, data=None):
-        self.data = data
+    def __init__(self, value=None):
+        self.value = value
         self.next = None
-class linked_list:
+class LinkedList:
     def __init__(self):
         self.head = None
-    def insert(self, data):
-        new_node = Node(data)
+    def insert(self, value):
+        new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
-        return data
-    def includes(self, data):
+        return value
+    def includes(self, value):
         if not self.head:
             return False
         cur = self.head
         while cur:
-            if cur.data == data:
+            if cur.value == value:
                 return True
             cur = cur.next
         return False
@@ -23,37 +23,41 @@ class linked_list:
         value = " "
         cur = self.head
         while cur.next != None:
-            value += " " + str(cur.data)
+            value += " " + str(cur.value)
             cur = cur.next
-        value += " " + str(cur.data)
+        value += " " + str(cur.value)
         print(value)
         return value
 
-    def insert_before(self, data, new_val):
-        if self.includes(data):
-            new_node = Node(data)
-            current = self.head
-            while current.next:
-                if current.next.data == data:
-                    new_node = current.next
-                    current.next = new_node.next
-                    return self.__str__()
-                else:
-                    current = current.next
-                    return self.__str__()
+    def insert_before(self, existing_value, value):
+        
+        current = self.head
+        if current.value == existing_value:
+            new_node = Node(value)
+            new_node.next = current
+            self.head = new_node
+            return True
+
+        while current.next:
+            if current.next.value == existing_value:
+                new_node = Node(value)
+                new_node.next = current.next
+                current.next = new_node
+                return True
+            current = current.next
 
 
-    def insert_after(self, existing, val):
-        if self.includes(existing):
-            current = self.head
-            while current:
-                if current.val == existing:
-                    new_node = current.next
-                    new_node.next = current.next
-                    return self.__str__()
-                else:
-                    current = current.next
-                    return self.__str__()
+
+    def insert_after(self, existing_value, value):
+        current = self.head
+        while current:
+            if current.value == existing_value:
+                new_node = Node(value)
+                new_node.next = current.next
+                current.next = new_node
+                return True
+
+            current = current.next    
 
     def append(self, value):
         if not self.head:
@@ -71,12 +75,24 @@ class linked_list:
         if self.head:
             current = self.head
             while current.next:
-                value_list.append(current.data)
+                value_list.append(current.value)
                 current = current.next
 
-            value_list.append(current.data)
+            value_list.append(current.value)
 
         if 0 <= k < len(value_list):
             return value_list[-(k+1)]
         else:
             return 'K is out of range'
+
+new_list = LinkedList()
+
+new_list.insert(1)
+new_list.insert(2)
+new_list.insert(3)
+new_list.insert_after(2, 4)
+new_list.insert_before(2, 5)
+new_list.insert_before(9, 7)
+new_list.insert_after(9,7)
+new_list.to_string()
+
