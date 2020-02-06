2f5730c3e0fe113c9ccd2d04a58c6c8f5b818445
glasscharlie <55407156+glasscharlie@users.noreply.github.com>
2020-02-06 12:14:12 -0800

    Update test_find_maximum_value.py

--- challenges/find-maximum-value/test_find_maximum_value.py
+++ challenges/find-maximum-value/test_find_maximum_value.py
@@ -1,57 +1,57 @@
 from find_maximum_value import _Node, BinaryTree
 
-# def test_empty():
-#     tree = BinaryTree()
-#     actual = tree.find_maximum_value()
-#     expected = None
-#     assert actual == expected
+def test_empty():
+    tree = BinaryTree()
+    actual = tree.find_maximum_value()
+    expected = None
+    assert actual == expected
 
-# def test_one():
-#     tree = BinaryTree()
-#     tree.add(11)
-#     actual = tree.find_maximum_value()
-#     expected = 11
-#     assert actual == expected
+def test_one():
+    tree = BinaryTree()
+    tree.add(11)
+    actual = tree.find_maximum_value()
+    expected = 11
+    assert actual == expected
 
-# def test_three():
-#     tree = BinaryTree()
-#     tree.add(5)
-#     tree.add(15)
-#     tree.add(3)
-#     actual = tree.pre_order()
-#     expected = 15
-#     assert actual == expected
+def test_three():
+    tree = BinaryTree()
+    tree.add(5)
+    tree.add(15)
+    tree.add(3)
+    actual = tree.pre_order()
+    expected = 15
+    assert actual == expected
 
-# def test_more():
-#     tree = BinaryTree()
-#     tree.add(22)
-#     tree.add(5)
-#     tree.add(3)
-#     tree.add(11)
-#     tree.add(2)
-#     tree.add(13)
-#     actual = tree.in_order()
-#     expected = 5
-#     assert actual == expected
+def test_more():
+    tree = BinaryTree()
+    tree.add(22)
+    tree.add(5)
+    tree.add(3)
+    tree.add(11)
+    tree.add(2)
+    tree.add(13)
+    actual = tree.in_order()
+    expected = 5
+    assert actual == expected
 
 
-# def test_multiples():
-#     tree = BinarySearchTree()
-#     tree.add(5)
-#     tree.add(3)
-#     tree.add(11)
-#     tree.add(2)
-#     tree.add(11)
-#     tree.add(11)
-#     tree.add(11)
-#     tree.add(11)
-#     tree.add(11)
-#     tree.add(11)
-#     tree.add(11)
-#     tree.add(11) 
-#     actual = tree.find_maximum_value()
-#     expected = 11
-#     assert actual == expected
+def test_multiples():
+    tree = BinarySearchTree()
+    tree.add(5)
+    tree.add(3)
+    tree.add(11)
+    tree.add(2)
+    tree.add(11)
+    tree.add(11)
+    tree.add(11)
+    tree.add(11)
+    tree.add(11)
+    tree.add(11)
+    tree.add(11)
+    tree.add(11) 
+    actual = tree.find_maximum_value()
+    expected = 11
+    assert actual == expected
 
 def test_add_four():
     tree = BinaryTree()
